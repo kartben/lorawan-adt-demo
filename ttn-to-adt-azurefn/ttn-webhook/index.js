3813716286b8e535ed2911b3c9f1beff842b671f
Benjamin Cabé <kartben@users.noreply.github.com>
2021-01-22 13:51:23 +0100

    Implement upsert when modelId is provided in the call

--- ttn-to-adt-azurefn/ttn-webhook/index.js
+++ ttn-to-adt-azurefn/ttn-webhook/index.js
@@ -13,24 +13,29 @@ module.exports = async function (context, req) {
     // a system-assigned managed identity and has "Azure Digital Twins Data Owner" permission on the ADT instance
     const serviceClient = new DigitalTwinsClient(url, credential);
     
-    const patches = [];
-
-    // TODO: it might be wise to retrieve the DTDL model of the device and only patch those properties
-    // that actually exist in the model, instead of all the properties found in the incoming decoded payload
+    var res;
+    if(decodedPayload['$metadata'] && decodedPayload['$metadata']['$model']) {
+        res = await serviceClient.upsertDigitalTwin(digitalTwinId, JSON.stringify(decodedPayload));
+    } else {
+        // TODO: it might be wise to retrieve the DTDL model of the device and only patch those properties
+        // that actually exist in the model, instead of all the properties found in the incoming decoded payload
+        
+        const patches = [];
+        Object.keys(decodedPayload).forEach(key => {
+            if(!key.startsWith('$') && decodedPayload[key] != null) {
+                patches.push({
+                    op: 'add',
+                    path: `/${key}`,
+                    value: decodedPayload[key]
+                })
+            }
+        });
+        
+        context.log(`Submitting the following patches to ${digitalTwinId}: `,patches)
+        // try to update an existing twin with this ID, hoping it has a matching model ¯\_(ツ)_/¯
+        res = await serviceClient.updateDigitalTwin(digitalTwinId, patches);
+    }
     
-    Object.keys(decodedPayload).forEach(key => {
-        if(decodedPayload[key] != null) {
-            patches.push({
-                op: 'add',
-                path: `/${key}`,
-                value: decodedPayload[key]
-            })
-        }
-    });
-    
-    context.log(`Submitting the following patches to ${digitalTwinId}: `,patches)
-
-    const res = await serviceClient.updateDigitalTwin(digitalTwinId, patches);
     
     context.res = {
         // status: 200, /* Defaults to 200 */
